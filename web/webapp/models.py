ad3ea45fcafc9a297d5b91a4d60c54645b423355
adityathakkar179 <aditya.thakkar1792000@gmail.com>
2020-07-20 16:54:18 +0530

    ok

--- web/webapp/models.py
+++ web/webapp/models.py
@@ -57,6 +57,240 @@ class BathroomFittings(models.Model):
     def __str__(self):
         return self.text1
 
+class ca(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class halfturn(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class healthFaucets(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class kitchenFaucets(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class alied(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class quarterturn(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class touch(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class shower(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class singleLever(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class snp(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class kitchensink(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class bathac(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class cistern(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class ecws(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class electronicflushing(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class kidsrange(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class seatcover(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class specialneedrange(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class urinals(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+
+class washbasins(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+
+class watersaving(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class bathtubs(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class bathtubswirlpool(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class customizeshowerpartition(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class pressurpump(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class showercubicles(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class showerpanels(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class showerpartition(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
+class steamshowerroom(models.Model):
+    image=models.ImageField(upload_to='images/')
+    text1=models.CharField(max_length=100)
+    text2=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.text1
+
 class tatawiron(models.Model):
     image=models.ImageField(upload_to='images/')
     text1=models.CharField(max_length=100)
--- web/webapp/views.py
+++ web/webapp/views.py
@@ -25,6 +25,128 @@ def about(request):
 def product(request):
     return render(request,'works.html')
 
+   
+def bff(request):
+    return render(request,'bf.html')
+
+def caa(request):
+    data21=ca.objects.all()
+    return render(request,'ca1.html',{'data21':data21})
+
+def hff(request):
+    data22=halfturn.objects.all()
+    return render(request,'hft.html',{'data22':data22})
+
+def haf(request):
+    data23=healthFaucets.objects.all()
+    return render(request,'hef.html',{'data23':data23})
+
+def kitchen(request):
+    data24=kitchenFaucets.objects.all()
+    return render(request,'kif.html',{'data24':data24})
+
+def ali(request):
+    data25=alied.objects.all()
+    return render(request,'oa.html',{'data25':data25})
+
+def qut(request):
+    data26=quarterturn.objects.all()
+    return render(request,'qt.html',{'data26':data26})
+
+def sens(request):
+    data27=touch.objects.all()
+    return render(request,'sensor.html',{'data27':data27})
+
+
+def sww(request):
+    data28=shower.objects.all()
+    return render(request,'sw.html',{'data28':data28})
+
+def sl(request):
+    data29=singleLever.objects.all()
+    return render(request,'slf.html',{'data29':data29})
+
+
+def snpp(request):
+    data30=snp.objects.all()
+    return render(request,'sp.html',{'data30':data30})
+
+def kits(request):
+    data31=kitchensink.objects.all()
+    return render(request,'ks.html',{'data31':data31})
+
+def bas(request):
+    data32=bathac.objects.all()
+    return render(request,'bathacss.html',{'data32':data32})
+
+def cit(request):
+    data33=cistern.objects.all()
+    return render(request,'cist.html',{'data33':data33})
+
+def ew(request):
+    data34=ecws.objects.all()
+    return render(request,'ea.html',{'data34':data34})
+
+def flu(request):
+    data35=electronicflushing.objects.all()
+    return render(request,'fl.html',{'data35':data35})
+
+def kr(request):
+    data36=kidsrange.objects.all()
+    return render(request,'kds.html',{'data36':data36})
+
+def stt(request):
+    data37=seatcover.objects.all()
+    return render(request,'seat.html',{'data37':data37})
+
+def sp(request):
+    data38=specialneedrange.objects.all()
+    return render(request,'special.html',{'data38':data38})
+
+def urr(request):
+    data39=urinals.objects.all()
+    return render(request,'uri.html',{'data39':data39})
+
+def bs(request):
+    data40=washbasins.objects.all()
+    return render(request,'wb.html',{'data40':data40})
+
+def sw(request):
+    data41=watersaving.objects.all()
+    return render(request,'ws.html',{'data41':data41})
+
+def btu(request):
+    data42=bathtubs.objects.all()
+    return render(request,'btub.html',{'data42':data42})
+
+def btuw(request):
+    data43=bathtubswirlpool.objects.all()
+    return render(request,'btubwirl.html',{'data43':data43})
+
+def cp(request):
+    data44=customizeshowerpartition.objects.all()
+    return render(request,'csp.html',{'data44':data44})
+
+def ppp(request):
+    data45=pressurpump.objects.all()
+    return render(request,'pp.html',{'data45':data45})
+
+def swcc(request):
+    data46=showercubicles.objects.all()
+    return render(request,'swc.html',{'data46':data46})
+
+def swpp(request):
+    data47=showerpanels.objects.all()
+    return render(request,'swp.html',{'data47':data47})
+
+def shpp(request):
+    data48=showerpartition.objects.all()
+    return render(request,'shp.html',{'data48':data48})
+
+def stm(request):
+    data49=steamshowerroom.objects.all()
+    return render(request,'steam.html',{'data49':data49})
+
 
 def contact(request):
     if request.method == 'POST':
